216f23260ba372cdc55815275852a1d49d9b2443
winnllam <winnieee.lam00@gmail.com>
2020-06-21 11:43:35 -0400

    SD-67: Added carousel on main page with rotating images

--- client-server/src/app/app.module.ts
+++ client-server/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +11,12 @@ import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, SignupComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    CarouselModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
