a485d087c92bd915a00ae0d31acf81c9b60c7918
winnllam <winnieee.lam00@gmail.com>
2020-06-21 23:43:31 -0400

    SD-67: Completed header and scrolling components

--- client-server/src/app/home/home.component.ts
+++ client-server/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { StarRatingComponent } from 'ng-starrating';
+import {
+  faArrowUp,
+  faArrowCircleDown,
+} from '@fortawesome/free-solid-svg-icons';
 import dishes from '../../assets/data/dishes.json';
 import stories from '../../assets/data/stories.json';
 
@@ -9,6 +13,11 @@ import stories from '../../assets/data/stories.json';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  isShow: boolean;
+  topPosToStartShowing = 100;
+  faArrowUp = faArrowUp;
+  faArrowCircleDown = faArrowCircleDown;
+
   totalStars: number = 5;
   dishes: any[];
   stories: any[];
@@ -19,4 +28,36 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  scrollDown() {
+    const newPosition = document.getElementById('scroll').offsetTop;
+    window.scroll({
+      top: newPosition,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
