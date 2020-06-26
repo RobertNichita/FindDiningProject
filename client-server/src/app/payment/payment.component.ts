edcb37beae3f1f1e22d5c6149889ec0b0dde25c6
Tony Cao <tony.cao1@outlook.com>
2020-06-26 13:40:31 -0400

    Tony: Added animations for the UI and fixed up id names

--- client-server/src/app/payment/payment.component.ts
+++ client-server/src/app/payment/payment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faCreditCard, faUser, faCalendarAlt, faKey, faHome, faBuilding, faCity } from '@fortawesome/free-solid-svg-icons';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-payment',
@@ -11,6 +13,12 @@ export class PaymentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init({
+      delay: 300,
+      duration: 1500,
+      once: false,
+      anchorPlacement: 'top-bottom',
+    });
   }
   faCreditCard = faCreditCard;
   faUser = faUser;
