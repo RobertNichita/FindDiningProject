65540684c579b0afe7fed98a9f644a6badbf470d
Tony Cao <tony.cao1@outlook.com>
2020-06-25 20:18:59 -0400

    Tony: Created payment UI page

--- client-server/src/app/app-routing.module.ts
+++ client-server/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'payment', component: PaymentComponent},
 ];
 
 @NgModule({
--- client-server/src/app/app.module.ts
+++ client-server/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { SignupComponent } from './signup/signup.component';
     DishCardComponent,
     OwnerCardComponent,
     CarouselComponent,
+    PaymentComponent,
   ],
   imports: [
     BrowserModule,
