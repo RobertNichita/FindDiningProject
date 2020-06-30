164c6b42ef28e99628770d40a71612868259d6c6
Calvin Cheng <calvin7889@LAPTOP-RP9AV23O.localdomain>
2020-06-30 19:11:09 -0400

    SD-79:Added Roles to SDUsers Models, enum file for Roles, and change roles method for SDUsers

--- server/auth2/views.py
+++ server/auth2/views.py
@@ -3,7 +3,14 @@ from auth2.models import SDUser
 import json
 
 
-def index(request):
+def signup_page(request):
 	body = json.loads(request.body)
-	user = SDUser.signup(body['nickname'], body['name'], body['picture'], body['updated_at'], body['email'], body['email_verified'])
+	user = SDUser.signup(body['nickname'], body['name'], body['picture'], body['updated_at'], body['email'], body['email_verified'], body['role'])
+	return HttpResponse(status=200)
+
+
+def reassign_page(request):
+	body = json.loads(request.body)
+	user = SDUser.objects.get(pk=body['email'])
+	user.reassign_role(body['role'])
 	return HttpResponse(status=200)
