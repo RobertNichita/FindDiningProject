ed333da9fb843c41c9a7f5bc9dcbf5de25dfc0ac
alacwong <46137625+alacwong@users.noreply.github.com>
2020-07-01 23:33:03 -0400

    SD-80: Updated cuisine_dict to about 2400 entries and updated scraping module to output cleaner results

--- server/SDServer/cuisine_dict/load_dict.py
+++ server/SDServer/cuisine_dict/load_dict.py
@@ -1,10 +1,13 @@
 # Load cuisine_dict into python set
 
 import csv
+import time
 data = {}
+start = time.time()
 
 with open('cuisine_dict.csv', newline='', encoding='utf-8-sig') as f:
-    data = {elem for line in list(csv.reader(f)) for elem in line}
+    data = {elem.lower() for line in list(csv.reader(f)) for elem in line}
 
 print(data)
-print(len(data))
+end = time.time()
+print(str(len(data)) + ' item loaded in ' + str(end - start) + ' s')
