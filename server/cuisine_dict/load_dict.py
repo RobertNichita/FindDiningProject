9f3c222827ccb59bad8d211f23f007602ce0e428
alacwong <46137625+alacwong@users.noreply.github.com>
2020-07-05 23:05:53 -0400

    SD-83: modified load_dict to compatible from any module

--- server/cuisine_dict/load_dict.py
+++ server/cuisine_dict/load_dict.py
@@ -1,28 +1,18 @@
 # Load cuisine_dict into python set
 
 import csv
-import time
 
 cuisine = {}
 dishes = {}
 
 
-def main(cuisine_path, dish_path):
-    start = time.time()
-    global cuisine, dishes
+def main(files):
+    file_data = []
 
-    with open(cuisine_path, newline='', encoding='utf-8-sig') as f:
-        cuisine = {elem.lower().strip() for line in list(csv.reader(f)) for elem in line}
+    for file in files:
+        with open(file, newline='', encoding='utf-8-sig') as f:
+            file_data.append({elem.lower().strip() for line in list(csv.reader(f)) for elem in line})
 
-    with open(dish_path, newline='', encoding='utf-8-sig') as f:
-        dishes = {elem.lower().strip() for line in list(csv.reader(f)) for elem in line}
-
-    end = time.time()
-    print(str(len(cuisine)) + ' cuisine items and ' + str(len(dishes)) +
-          ' dish items loaded in ' + str(end - start) + ' s')
+    return file_data
 
 
-if __name__ == '__main__ ':
-    main('cuisine.csv', 'dishes.csv')
-else:
-    main('cuisine_dict/cuisine.csv', 'cuisine_dict/dishes.csv')
