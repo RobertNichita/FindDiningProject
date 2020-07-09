5c3016102b97a2f9412ae1d3899c34109da882a8
Tony Cao <tony.cao1@outlook.com>
2020-07-09 15:22:48 -0400

    SD-92: Added function to get user role in login service

--- client-server/src/app/service/login.service.ts
+++ client-server/src/app/service/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -34,5 +35,19 @@ export class LoginService {
       role: 'RO',
     };
     this.http.post<any>(endpoint, userObject).subscribe((data) => {});
+  }
+
+  /*
+  @Input: JSON object - source section of user profile
+  @Output: None
+
+  Assign the 'Restauraut Owner' role to the user using their email.
+  */
+  getUserRole(userData): Observable<any> {
+    const endpoint = `${LoginService.AUTH_ENDPOINT}/data/`;
+    const userObject = {
+      email: userData._value.email,
+    };
+    return this.http.get(endpoint, { params: userObject });
   }
 }
