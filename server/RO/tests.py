c404de268830ad70c2eeba4458f1b5cd48986297
alacwong <46137625+alacwong@users.noreply.github.com>
2020-07-08 20:30:40 -0400

    SD-83: Added documentation and fixed test cases for api

--- server/RO/tests.py
+++ server/RO/tests.py
@@ -63,17 +63,17 @@ class RestaurantTestCase(TestCase):
 
     def test_find(self):
         c = Client()
-        response = c.get('/RO/get/', {'_id': '111111111111111111111111'},
+        response = c.get('/api/RO/get/', {'_id': '111111111111111111111111'},
                          content_type="application/json")
         self.assertDictEqual(self.expected, json.loads(response.content))
 
     def test_find_all(self):
         c = Client()
-        response = c.get('/RO/getAll/')
+        response = c.get('/api/RO/getAll/')
         expected = [self.expected, self.expected2]
         self.assertListEqual(expected, json.loads(response.content)['Restaurants'])
 
     def test_insert(self):
         c = Client()
-        response = c.post('/RO/insert/', self.expected3, content_type="application/json" )
+        response = c.post('/api/RO/insert/', self.expected3, content_type="application/json" )
         self.assertDictEqual(self.expected3, json.loads(response.content))
