917a89c55e1dcd386772722e15c615d673abd041
Tony Cao <tony.cao1@outlook.com>
2020-07-10 19:01:53 -0400

    SD-97: Added graphics to dashboard when no orders present

--- client-server/src/app/pages/restuarant-dashboard/restuarant-dashboard.component.ts
+++ client-server/src/app/pages/restuarant-dashboard/restuarant-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import orders from '../../../assets/data/orders.json';
+import { ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-restuarant-dashboard',
@@ -7,8 +8,30 @@ import orders from '../../../assets/data/orders.json';
   styleUrls: ['./restuarant-dashboard.component.scss'],
 })
 export class RestuarantDashboardComponent implements OnInit {
-  orders = orders;
-  constructor() {}
+  new_orders: any[];
+  in_progress: any[];
+  complete: any[];
+  orders: any[];
+
+  constructor() {
+    this.orders = orders;
+    this.new_orders = [];
+    this.in_progress = [];
+    this.complete = [];
+    for (let i = 0; i < this.orders.length; i++) {
+      if (this.orders[i].AccDec_Timestamp === '') {
+        this.new_orders.push(this.orders[i]);
+      } else if (
+        this.orders[i].AccDec_Timestamp !== '' &&
+        this.orders[i].Complete_Timestamp === ''
+      ) {
+        this.in_progress.push(this.orders[i]);
+      } else {
+        this.complete.push(this.orders[i]);
+      }
+    }
+    console.log(this.new_orders);
+  }
 
   ngOnInit(): void {}
 }
