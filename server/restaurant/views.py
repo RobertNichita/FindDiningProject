c6ee6c2800e1e8d202004c06eff5fcc4570bd1e7
Calvin Cheng <calvincheng2000@gmail.com>
2020-07-17 20:38:16 -0400

    SD-100: Reconfigured Endpoints (Including Urls, Tests, and Documentation)

--- server/restaurant/views.py
+++ server/restaurant/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse, JsonResponse
-from restaurant.models import Food, ManualTag
+from restaurant.models import Food, ManualTag, Restaurant
 from django.forms.models import model_to_dict
 import json
 
 
-def add_tag_page(request):
+def insert_tag_page(request):
     body = json.loads(request.body)
     tag = ManualTag.add_tag(body['food_name'], body['restaurant_id'], body['category'], body['value'])
     return JsonResponse(model_to_dict(tag))
@@ -16,9 +16,8 @@ def clear_tags_page(request):
     return HttpResponse(status=200)
 
 
-def get_food_by_restaurant_page(request):
+def get_dish_by_restaurant_page(request):
     rest_id = request.GET.get('restaurant_id')
-
     return JsonResponse(Food.get_by_restaurant(rest_id))
 
 
@@ -26,13 +25,51 @@ def all_dishes_page(request):
     return JsonResponse(Food.get_all())
 
 
-def create_dish_page(request):
+def insert_dish_page(request):
     body = json.loads(request.body)
     food = Food.add_dish(body)
     food._id = str(food._id)
     return JsonResponse(model_to_dict(food))
 
-def auto_tag(request):
+
+def auto_tag_page(request):
     body = json.loads(request.body)
     tags = [model_to_dict(tag) for tag in ManualTag.auto_tag_food(body['_id'])]
-    return JsonResponse({'tags' : tags})
+    return JsonResponse({'tags': tags})
+
+
+def get_restaurant_page(request):
+    try:  # body
+        body = json.loads(request.body)
+        _id = body['_id']
+    except:  # query string
+        _id = request.GET.get('_id')
+
+    restaurant = Restaurant.get(_id)
+    if restaurant:
+        return JsonResponse(model_to_dict(restaurant))
+    else:
+        return JsonResponse({})
+
+
+def get_all_restaurants_page(request):
+    return JsonResponse(Restaurant.get_all())
+
+
+def insert_restaurant_page(request):
+    restaurant = Restaurant.insert(json.loads(request.body))
+    restaurant._id = str(restaurant._id)
+    return JsonResponse(model_to_dict(restaurant))
+
+
+def edit_restaurant_page(request):
+    body = json.loads(request.body)
+    restaurant = Restaurant.get(body["restaurant_id"])
+    del body['restaurant_id']
+    for field in body:
+        if body[field] != "":
+            setattr(restaurant, field, body[field])
+    restaurant.clean_fields()
+    restaurant.clean()
+    restaurant.save()
+    return HttpResponse(status=200)
---
+++ server/user/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse, JsonResponse
+from user.models import SDUser
+from restaurant.models import Restaurant
+import json
+
+
+def signup_page(request):
+    body = json.loads(request.body)
+    user = SDUser.signup(nickname=body['nickname'], name=body['name'], picture=body['picture'],
+                         updated=body['updated_at'], email=body['email'],
+                         verified=body['email_verified'], role=body['role'], restaurant_id=body['restaurant_id'])
+    return HttpResponse(status=200)
+
+
+def reassign_page(request):
+    body = json.loads(request.body)
+    user = SDUser.objects.get(pk=body['user_email'])
+    user.reassign_role(body['role'])
+    if body['role'] == "RO":
+        del body['user_email']
+        del body['role']
+        restaurant = Restaurant.insert(body)
+        user.restaurant_id = str(restaurant._id)
+        user.save(update_fields=["restaurant_id"])
+        return JsonResponse({"restaurant_id": str(restaurant._id)})
+    else:
+        user.restaurant_id = None
+        user.save(update_fields=["restaurant_id"])
+    return HttpResponse(status=200)
+
+
+def data_page(request):
+    req_email = request.GET.get('email')
+    user = SDUser.objects.get(pk=req_email)
+    return JsonResponse(
+        {'nickname': user.nickname, 'name': user.name, 'picture': user.picture, 'updated_at': user.last_updated,
+         'email': user.email, 'email_verified': user.email_verified, 'role': user.role})
+
+
+def exists_page(request):
+    req_email = request.GET.get('email')
+    return JsonResponse({'exists': SDUser.objects.filter(email=req_email).exists()})
