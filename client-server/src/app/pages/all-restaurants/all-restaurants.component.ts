309eef073b90b11a3c3c95ccbe47f34930715ac7
Winnie <36285522+winnllam@users.noreply.github.com>
2020-07-30 17:00:19 -0400

    Bugfix - Dynamic Search Bar (#95)
    * Bugfix: Modified menu setup and edit to be dynamic
    
    * Bugfix: Fixed search bar to have dynamic search
    
    * Bugfix: Added test documentation for search bar

--- client-server/src/app/pages/all-restaurants/all-restaurants.component.ts
+++ client-server/src/app/pages/all-restaurants/all-restaurants.component.ts
@@ -13,8 +13,14 @@ export class AllRestaurantsComponent implements OnInit {
   userId: string = '';
   role: string = '';
 
+  allRestaurants: any[];
+  allDishes: any[];
+
   restaurants: any[];
   dishes: any[];
+  inputRestaurant: string = '';
+  inputDishes: string = '';
+
   faSearch = faSearch;
 
   constructor(
@@ -31,14 +37,67 @@ export class AllRestaurantsComponent implements OnInit {
     this.data.changeUserId(this.userId);
     this.data.changeRole(this.role);
 
-    // Get list of all restaurants
+    this.loadRestaurants();
+    this.loadDishes();
+  }
+
+  loadRestaurants() {
     this.restaurantsService.listRestaurants().subscribe((data) => {
       this.restaurants = data.Restaurants;
-    });
-    this.restaurantsService.getDishes().subscribe((data) => {
-      this.dishes = data.Dishes;
+      this.allRestaurants = data.Restaurants;
     });
   }
 
-  displayList(list) {}
+  loadDishes() {
+    this.restaurantsService.getDishes().subscribe((data) => {
+      this.dishes = data.Dishes;
+      this.allDishes = data.Dishes;
+    });
+  }
+
+  filterList(list, filter) {}
+
+  searchRestaurants() {
+    if (this.inputRestaurant == '') {
+      this.restaurants = this.allRestaurants;
+    } else {
+      this.restaurants = [];
+      for (var i = 0; i < this.allRestaurants.length; i++) {
+        var query = this.allRestaurants[i];
+        if (
+          query.name
+            .toLowerCase()
+            .includes(this.inputRestaurant.toLowerCase()) ||
+          query.cuisine
+            .toLowerCase()
+            .includes(this.inputRestaurant.toLowerCase()) ||
+          query.pricepoint
+            .toLowerCase()
+            .includes(this.inputRestaurant.toLowerCase()) ||
+          query.owner_name
+            .toLowerCase()
+            .includes(this.inputRestaurant.toLowerCase())
+        ) {
+          this.restaurants.push(query);
+        }
+      }
+    }
+  }
+
+  searchDishes() {
+    if (this.inputDishes == '') {
+      this.dishes = this.allDishes;
+    } else {
+      this.dishes = [];
+      for (var i = 0; i < this.allDishes.length; i++) {
+        var query = this.allDishes[i];
+        if (
+          query.name.toLowerCase().includes(this.inputDishes.toLowerCase()) ||
+          query.price.toLowerCase().includes(this.inputDishes.toLowerCase())
+        ) {
+          this.dishes.push(query);
+        }
+      }
+    }
+  }
 }
