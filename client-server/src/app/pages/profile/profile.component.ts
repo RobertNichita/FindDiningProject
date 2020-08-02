ba498d78c4d51585dcbb8e42683665bbfe2252de
Tony Cao <59297928+t-cao19@users.noreply.github.com>
2020-08-02 14:23:42 -0400

    Bugfix - Profile UI (#110)
    * bugfix: Removed JSON Schema and replaced with db info
    
    * bugfix: created edit user info modal
    
    * bugfix: removed window.reload() for now
    
    * bugfix: fixed edit info modal to dynamically update
    
    * bugfix: Added photo illustration in bottom right corner
    
    * bugfix: added the actual photo
    
    * bugfix: Added media queries for the profile page
    
    * bugfix: added documentation for profile UI tests
    
    * bugfix: removed email and password from documentation

--- client-server/src/app/pages/profile/profile.component.ts
+++ client-server/src/app/pages/profile/profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginService } from 'src/app/service/login.service';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +11,67 @@ import { AuthService } from '../../auth/auth.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor(public auth: AuthService) {}
+  userId: string = '';
+  userData: any;
+  modalRef: any;
+  faCalendar = faCalendar;
 
-  ngOnInit() {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public auth: AuthService,
+    private loginService: LoginService,
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit() {
+    this.userId = sessionStorage.getItem('userId');
+    this.getUserInfo();
+  }
+
+  openEditModal(content) {
+    this.modalRef = this.modalService.open(content);
+  }
+
+  updateProfile() {
+    var userInfo = {
+      email: this.userId,
+      name: (<HTMLInputElement>document.getElementById('name')).value,
+      address: (<HTMLInputElement>document.getElementById('address')).value,
+      phone: (<HTMLInputElement>document.getElementById('phone')).value,
+      birthday: (<HTMLInputElement>document.getElementById('dateOfBirth'))
+        .value,
+    };
+
+    if (userInfo.birthday == '') {
+      userInfo.birthday = null;
+    }
+    if (userInfo.phone == '') {
+      userInfo.phone = null;
+    }
+
+    if (
+      (userInfo.phone != null && userInfo.phone.length != 10) ||
+      (userInfo.birthday != null &&
+        !userInfo.birthday.match('^\\d{4}-\\d{2}-\\d{2}$')) ||
+      !userInfo.name
+    ) {
+      alert(
+        'Please ensure formats are proper. Name should not empty, phone numbers should be 10 digits with no dashes and birthday should be YYYY-MM-DD'
+      );
+    } else {
+      this.loginService.editUser(userInfo);
+      this.modalRef.close();
+      this.getUserInfo();
+      setTimeout(function () {
+        window.location.reload();
+      });
+    }
+  }
+
+  getUserInfo() {
+    this.loginService.getUser({ email: this.userId }).subscribe((data) => {
+      this.userData = data;
+    });
+  }
 }
